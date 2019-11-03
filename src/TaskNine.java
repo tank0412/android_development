addca2a47e550b1e54d2851956da590ba9de95aa
Roman <romabukhtiarov@gmail.com>
2019-11-03 13:32:50 +0300

    Impl task nine

---
+++ src/TaskNine.java
@@ -0,0 +1,20 @@
+public class TaskNine {
+}
+
+class Student {}
+
+class Prepodavatel {}
+
+class Professor extends Prepodavatel {}
+
+class Assistent extends Prepodavatel {}
+
+class Magistrant extends Student {}
+
+class Aspirant extends Student {}
+
+class Dekan extends Rukovoditel {}
+
+class Rektor extends Rukovoditel {}
+
+class Rukovoditel {}
