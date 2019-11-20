8a102a04dbc8f386892400d40fe711eb3ffc7fb1
Roman <romabukhtiarov@gmail.com>
2019-11-20 10:10:58 +0300

    Impl display one picture from url

--- app/src/main/java/com/example/lab3ad/ListAdapter.java
+++ app/src/main/java/com/example/lab3ad/ListAdapter.java
@@ -1,38 +1,67 @@
 package com.example.lab3ad;
 
+import android.content.Context;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
 public class ListAdapter extends RecyclerView.Adapter {
-    public ImageView image;
+    /*
+    private List<String> imagesList;
+
+    public ListAdapter(Context context, List<String> imagesList) {
+        this.imagesList = imagesList;
+    }
+     */
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener, View.OnClickListener {
+
+        public ViewHolder(View view) {
+            super(view);
+            view.setOnCreateContextMenuListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        }
+    }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        /*
-        // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.activity_main, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main, parent, false);
+        ViewHolder vh = new ViewHolder(view);
 
-        ListAdapter vh = new ListAdapter(v);
         return vh;
-        */
 
-         return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+        View v = holder.itemView;
+        ImageView image = v.findViewById(R.id.photo);
+        Picasso.get().load("https://i.imgur.com/2KxoIsa.jpg").into(image);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 1;
     }
 }
--- app/src/main/java/com/example/lab3ad/MainActivity.java
+++ app/src/main/java/com/example/lab3ad/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -15,29 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
+
         RecyclerView recyclerView = findViewById(R.id.show_Photos); // для прокручиваемого списка
         LinearLayoutManager layoutManager = new LinearLayoutManager(this); // similar to listView
         recyclerView.setLayoutManager(layoutManager);
 
         ListAdapter listAdapter = new ListAdapter();
         recyclerView.setAdapter(listAdapter);
-         */
 
-        // получаем экземпляр элемента ListView
-        ListView listView = findViewById(R.id.listView);
-
-// определяем строковый массив
-        final String[] catNames = new String[] {
-                "Рыжик", "Барсик", "Мурзик", "Мурка", "Васька",
-                "Томасина", "Кристина", "Пушок", "Дымка", "Кузя",
-                "Китти", "Масяня", "Симба"
-        };
-
-// используем адаптер данных
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, catNames);
-
-        listView.setAdapter(adapter);
     }
 }
