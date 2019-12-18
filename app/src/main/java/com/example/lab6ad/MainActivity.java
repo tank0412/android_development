1ae106e8f24b16ad92a6f8562ea9dab8208b10b6
Roman <romabukhtiarov@gmail.com>
2019-12-18 20:31:11 +0300

    Remove home fragment

--- app/src/main/java/com/example/lab6ad/MainActivity.java
+++ app/src/main/java/com/example/lab6ad/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_video, R.id.nav_about,
+                R.id.nav_video, R.id.nav_about,
                 R.id.nav_playlist)
                 .setDrawerLayout(drawer)
                 .build();
